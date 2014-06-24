20a5cff5a8246406ef4d5432da94c7c7c15abe6b
Justin Anastos <janastos@useallfive.com>
2014-06-24 10:47:21 -0400

    Origination.

---
+++ Gruntfile.js
@@ -0,0 +1,96 @@
+var config;
+var matchdep = require('matchdep');
+var module;
+
+config = {
+    files: {
+        build: 'lib/marionette.regionChildCloseEvent.js',
+        check: [
+            'lib/marionette.regionChildCloseEvent.js',
+            'Gruntfie.js'
+        ],
+        dest: 'dist/marionette.regionChildCloseEvent.min.js',
+        docs: 'docs'
+    }
+};
+
+module.exports = function(grunt) {
+
+    var pkg = grunt.file.readJSON('package.json');
+    var defaultTasks;
+
+    grunt.initConfig({
+        pkg: pkg,
+
+        clean: {
+            dist: 'dist',
+            docs: 'docs'
+        },
+
+        jshint: {
+            options: {
+                jshintrc: true
+            },
+            all: {
+                files: {
+                    src: config.files.check
+                }
+            }
+        },
+
+        jscs: {
+            all: {
+                files: {
+                    src: config.files.check
+                }
+            }
+        },
+
+        groc: {
+            options: {
+                out: config.files.docs,
+                strip: 'lib/'
+            },
+            all: {
+                src: config.files.build
+            },
+            github: {
+                options: {
+                    github: true
+                },
+                src: config.files.build
+            }
+        },
+
+        uglify: {
+            all: {
+                src: config.files.build,
+                dest: config.files.dest
+            }
+        },
+
+        watch: {
+            options: {
+                interrupt: true
+            },
+            js: {
+                files: config.files.check,
+                tasks: ['clean', 'uglify', 'groc:all']
+            }
+        }
+    });
+
+    // Load all npm dependencies.
+    matchdep.filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    // Register tasks.
+    defaultTasks = [
+        'clean',
+        'jshint',
+        'jscs',
+        'groc:all',
+        'uglify'
+    ];
+    grunt.registerTask('default', defaultTasks);
+    grunt.registerTask('dev', defaultTasks.concat('watch'));
+};
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-marionette.regionChildCloseEvent
-================================
+# marionette.regionChildCloseEvent
+
+Brought to you by [Use All Five, Inc.](http://www.useallfive.com)
+
+```
+Author: Justin Anastos <janastos@useallfive.com>
+Author URI: [http://www.useallfive.com](http://www.useallfive.com)
+Repository: https://github.com/UseAllFive/marionette.regionChildCloseEvent
+```
+
+## Scope
+
+Have [Marionette Regions](https://github.com/marionettejs/backbone.marionette/blob/master/docs/marionette.region.md) fire an event `child:view:closed` when a child view is closed.
+
+
+## Note
+Grunt will automatically generate annotated source code. It uses [groc](https://github.com/nevir/groc), which
+requires you to install Pygments:
+
+```
+pip install Pygments
+```
